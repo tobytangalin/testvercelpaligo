d7d7d947cfc6d0a842ebfc9bc5049377f7c78ef9
Toby Tangalin <97184147+tobytangalin@users.noreply.github.com>
2023-09-01 11:44:47 +0200

    for Louise

--- en/js/toc.js
+++ en/js/toc.js
@@ -1,1 +1,12 @@
-var toc = [];
+var toc = [{
+"url": "add-team-members-and-manage-security-roles.html",
+"text": "Add team members",
+"class": "topic-link section"
+,
+"children": [
+{
+"url": "roles-and-permissions-in-sitecore-send.html",
+"text": "Security roles",
+"class": "topic-link section"
+}]
+}];
